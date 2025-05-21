ffd25569ce8a2d33acc9c8499ef2e7286092643b
Dip Dey <dipdey993@gmail.com>
2025-05-21 11:56:57 +0600

    fix: fixed backend to serve static files

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from quart import Quart, jsonify, render_template
+import os
+from quart import Quart, jsonify, render_template,send_from_directory
 from src.api import api_bp
 
 app = Quart(__name__)
@@ -8,5 +9,12 @@ app.register_blueprint(api_bp)
 async def index():
     return await render_template("index.html")
 
+@app.route('/<folder>/<path:filename>')
+async def serve_assets(folder, filename):
+    path = os.path.join(f'templates/{folder}', filename)
+    if not os.path.isfile(path):
+        return {"error": "File not found"}, 404
+    return await send_from_directory(f'templates/{folder}', filename)
+    
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=8080)
